5eaeac475996ea89415aa275c5620fb568092356
duc.vu <duc.vu@sotatek.com>
2022-05-22 11:43:46 +0700

    Remove uneccessary file

--- src/services/web3.ts
+++ src/services/web3.ts
@@ -1,32 +1,16 @@
 import Web3 from 'web3';
-import { connectorNames, ConnectorNames, connectorsByName } from '../constants/connectors';
-import { ETH_CHAIN_ID, POLYGON_CHAIN_ID } from '../constants/network';
-import {NETWORK_AVAILABLE} from "../constants";
+import { NETWORK_AVAILABLE } from '../constants';
 
 const POOL_ABI = require('../abi/Pool.json');
 
-const NETWORK_URL = process.env.REACT_APP_NETWORK_URL || "";
-const POLYGON_NETWORK_URL = process.env.REACT_APP_POLYGON_RPC_URL || "";
-export const MAX_INT = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL || '';
+const POLYGON_NETWORK_URL = process.env.REACT_APP_POLYGON_RPC_URL || '';
+export const MAX_INT = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 
 export enum SmartContractMethod {
-  Write = "Write",
-  Read = "Read"
+  Write = 'Write',
+  Read = 'Read',
 }
-
-type smartContractMethod = Extract<SmartContractMethod, SmartContractMethod.Write | SmartContractMethod.Read>
-
-export const getWeb3Instance = () => {
-  const windowObj = window as any;
-  const { ethereum, web3 } = windowObj;
-  if (ethereum && ethereum.isMetaMask) {
-    return new Web3(ethereum);
-  }
-  if (web3) {
-    return new Web3(web3.currentProvider);
-  }
-  return null;
-};
 
 export const isMetaMaskInstalled = () => {
   const windowObj = window as any;
@@ -34,87 +18,25 @@ export const isMetaMaskInstalled = () => {
   return ethereum && ethereum.isMetaMask;
 };
 
-
-export const getProviderByNetwork = (
-  networkName: connectorNames,
-  appChainID: string,
-  typeMethod: smartContractMethod,
-  forceUsingEther: boolean
-) => {
-  if (forceUsingEther) {
-      return new Web3.providers.HttpProvider(NETWORK_URL);
-  }
-
-  if (appChainID && typeMethod === SmartContractMethod.Read) {
-      switch (appChainID) {
-        case POLYGON_CHAIN_ID:
-          return new Web3.providers.HttpProvider(POLYGON_NETWORK_URL);
-        case ETH_CHAIN_ID:
-        default:
-          return new Web3.providers.HttpProvider(NETWORK_URL);
-      }
-  }
-
-  const provider = (connectorsByName[networkName as connectorNames] as any);
-  return provider;
-}
-
-export const getContractInstance =
-  (ABIContract: any,
-   contractAddress: string,
-   networkName: connectorNames = ConnectorNames.MetaMask,
-   appChainID: string = ETH_CHAIN_ID as string,
-   typeMethod: smartContractMethod = SmartContractMethod.Read,
-   forceUsingEther: boolean = false
-  ) => {
-  const provider = getProviderByNetwork(networkName as connectorNames, appChainID, typeMethod, forceUsingEther);
+export const getContractInstance = (ABIContract: any, contractAddress: string) => {
+  const provider = (window as any).ethereum;
 
   if (provider) {
     const web3Instance = new Web3(provider);
 
-    return new web3Instance.eth.Contract(
-      ABIContract,
-      contractAddress,
-    );
+    return new web3Instance.eth.Contract(ABIContract, contractAddress);
   }
 
   return;
 };
 
-export const getContractReadInstance = (ABIContract: any, contractAddress: string, networkAvailable : string ) => {
-  let provider;
-  switch (networkAvailable) {
-    // case NETWORK_AVAILABLE.BSC:
-    //   provider = new Web3.providers.HttpProvider(BSC_NETWORK_URL);
-    //   break;
-    
-    case NETWORK_AVAILABLE.POLYGON:
-      provider = new Web3.providers.HttpProvider(POLYGON_NETWORK_URL);
-      break;
-
-    case NETWORK_AVAILABLE.ETH:
-      provider = new Web3.providers.HttpProvider(NETWORK_URL);
-      break;
-  }
-  if (!provider) {
-    return
-  }
-
-  const web3Instance = new Web3(provider);
-
-  return new web3Instance.eth.Contract(
-    ABIContract,
-    contractAddress,
-  );
-};
-
-export const getContractInstanceWeb3 = (networkAvailable : string ) => {
+export const getContractInstanceWeb3 = (networkAvailable: string) => {
   let provider;
   switch (networkAvailable) {
     // case NETWORK_AVAILABLE.BSC:
     //   provider = new Web3.providers.HttpProvider(BSC_NETWORK_URL);
     //   return new Web3(provider);
-    
+
     case NETWORK_AVAILABLE.POLYGON:
       provider = new Web3.providers.HttpProvider(POLYGON_NETWORK_URL);
       return new Web3(provider);
@@ -151,39 +73,14 @@ export const getContractInstanceWithEthereum = (ABIContract: any, contractAddres
   }
 };
 
-export const getContractInstanceWithBSC = (ABIContract: any, contractAddress: string) => {
-  const windowObj = window as any;
-  const { ethereum } = windowObj;
-  const web3Instance = new Web3(ethereum);
-  return new web3Instance.eth.Contract(ABIContract, contractAddress);
-};
-
 export const convertFromWei = (value: any, unit = 'ether') => {
   return Web3.utils.fromWei(value);
 };
 
-export const convertToWei = (value: any, unit = 'ether') => {
-  // const webInstance = getWeb3Instance();
-  // // @ts-ignore
-  // return webInstance.utils.toWei(value, unit);
-  return Web3.utils.toWei(value);
-};
-
 export const isValidAddress = (address: string) => {
   return Web3.utils.isAddress(address);
-}
-
-export const getETHBalance = async (loginUser: string) => {
-  const web3 = getWeb3Instance() as any;
-  if (web3) {
-    const balance = await web3.eth.getBalance(loginUser);
-
-    return web3.utils.fromWei(balance);
-  };
-
-  return 0;
-}
+};
 
 export const convertToBN = (number: string) => {
-  return Web3.utils.toBN(number)
-}
+  return Web3.utils.toBN(number);
+};
