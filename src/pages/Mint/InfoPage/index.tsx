d9c1a0a12b37c95acd67c9e5411fa4b7c1a86aba
Sotatek-HienPham3 <hien.pham3@sotatek.com>
2022-05-23 11:44:24 +0700

    fix conflict

--- src/pages/Mint/InfoPage/index.tsx
+++ src/pages/Mint/InfoPage/index.tsx
@@ -4,7 +4,6 @@ import { useMint } from 'hooks/useMint';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { alert } from 'store/actions/alert';
-import { unixToDate } from 'utils/convertDate';
 import { BorderOutline } from '../BorderOutline';
 import Countdown from '../Countdown';
 import MintFormContainer from '../MintForm';
