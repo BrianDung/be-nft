0a983d7cde1568fba7ddc555e55e9aa5b3a3c929
BrianTran <tran.tiendung96hust@gmail.com>
2022-05-23 11:10:06 +0700

    fix: delete unnecessary file

--- src/constants/upload.ts
+++
@@ -1,5 +0,0 @@
-const UPLOAD_ACC1 = process.env.REACT_APP_UPLOAD_ACC1 || '';
-const UPLOAD_ACC2 = process.env.REACT_APP_UPLOAD_ACC2 || '';
-const UPLOAD_ACC3 = process.env.REACT_APP_UPLOAD_ACC3 || '';
-
-export const ACCEPT_UPLOAD = new Set([UPLOAD_ACC1, UPLOAD_ACC2, UPLOAD_ACC3]);
