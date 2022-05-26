492e2597043416e27c0d25244639d34d4ebf5baf
duc.vu <duc.vu@sotatek.com>
2022-05-26 15:05:05 +0700

    Update sold progress logic

--- src/pages/Mint/SoldProgress/index.tsx
+++ src/pages/Mint/SoldProgress/index.tsx
@@ -1,4 +1,3 @@
-import { useWeb3React } from '@web3-react/core';
 import BigNumber from 'bignumber.js';
 import { MESSAGES } from 'constants/mint';
 import { useMint } from 'hooks/useMint';
@@ -14,16 +13,13 @@ interface SoldProgressProps {}
 const SoldProgress = (props: SoldProgressProps) => {
   const styles = useStyles();
   const [totalSupply, setTotalSupply] = useState<number>(0);
-  const { chainId } = useWeb3React();
   const { getTotalSupply } = useMint();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const interval = setInterval(async () => {
-      if (totalSupply >= 5500) {
-        clearInterval(interval);
-      }
+    let timeoutId: any = null;
 
+    async function retrieveTotalSupply() {
       const requestTotalSupply = await getTotalSupply();
 
       dispatch({
@@ -33,25 +29,29 @@ const SoldProgress = (props: SoldProgressProps) => {
         },
       });
 
-      setTotalSupply(Number(requestTotalSupply));
-    }, 2000);
+      setTotalSupply(() => Number(requestTotalSupply));
+      clearTimeout(timeoutId);
+
+      if (requestTotalSupply >= 5500) {
+        dispatch(alert(MESSAGES.SOLD_OUT));
+        return;
+      }
+
+      timeoutId = setTimeout(() => retrieveTotalSupply(), 2000);
+    }
+
+    retrieveTotalSupply();
 
     return () => {
-      clearInterval(interval);
+      clearTimeout(timeoutId);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chainId]);
-
-  useEffect(() => {
-    if (totalSupply >= 5500) {
-      dispatch(alert(MESSAGES.SOLD_OUT));
-    }
-  }, [dispatch, totalSupply]);
+  }, []);
 
   const progress =
     totalSupply >= 5500
       ? 100
-      : new BigNumber((totalSupply === 0 ? 1 : totalSupply)).div(5500).multipliedBy(100).toNumber();
+      : new BigNumber(totalSupply === 0 ? 0 : totalSupply).div(5500).multipliedBy(100).toNumber();
 
   return (
     <div>
@@ -60,12 +60,10 @@ const SoldProgress = (props: SoldProgressProps) => {
       <div className={styles.soldProgress}>
         <div className={styles.jubValue}>
           <div className={styles.leftBotSec}>{totalSupply >= 5500 ? 100 : progress.toFixed(2)}% of Xborg Sold</div>
-          <div className={styles.rightBotSec}>{totalSupply}/5500</div>
+          <div className={styles.rightBotSec}>{totalSupply >= 5500 ? 5500 : totalSupply}/5500</div>
         </div>
         <BorderOutline>
-          <div className={styles.progress}
-           style={{padding: `${progress <= 0.4 ? '6px 0' : '4px 0'}`}}
-           >
+          <div className={styles.progress} style={{ padding: `${progress <= 0.4 ? '6px 0' : '4px 0'}` }}>
             <div
               className={`${styles.achieved}  ${progress === 100 ? styles.progressFull : ''}`}
               style={{ width: `${progress}%` }}
