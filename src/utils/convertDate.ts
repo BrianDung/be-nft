9d9b9e9fa12618a0329103c14dad35dab1d097e9
developer <any.io>
2023-04-07 03:59:08 +0700

    feat: update code

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,5 +1,29 @@
-export const unixToDate = (time: number|string) =>{
+import { MintTimeLine } from 'constants/mint';
+import moment from 'moment';
+
+export const unixToDate = (time: number | string) => {
   return new Date(Number(time) * 1000);
-}
+};
 
 export const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export enum Rounds {
+  NotSet,
+  WhiteList,
+  Public,
+  Minting,
+}
+
+export const CheckCurrentRound = (saleStage: MintTimeLine, mintState: boolean) => {
+  const startPublicSale = moment().isAfter(unixToDate(process.env.REACT_APP_START_PUBLIC_SALE as string));
+  const endPublicSale = moment().isBefore(process.env.REACT_APP_END_PUBLIC_SALE as string);
+  if (saleStage === MintTimeLine.NotSet && !mintState) {
+    return Rounds.NotSet;
+  } else if (saleStage <= MintTimeLine.WLMintPhase3 && saleStage >= MintTimeLine.WLMintPhase1 && !mintState) {
+    return Rounds.WhiteList;
+  } else if (saleStage > MintTimeLine.WLMintPhase3 && !mintState && startPublicSale && endPublicSale) {
+    return Rounds.Public;
+  } else {
+    Rounds.Minting;
+  }
+};
